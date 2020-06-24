5f75f55c86a6116e4ff0f294fbcededffb396a01
Abigail Grundy <abigial.grundy@bbc.co.uk>
2020-06-24 16:36:45 +0100

    step 2 nearly finished

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,13 +3,35 @@
 function fizzbuzz() {
   console.log("Welcome to Fizzbuzz");
   //put your code here
-  for (let i = 0; i <= 100; i++) {
+  for (let i = 0; i <= 70; i++) {
     let result = "";
+
     if (i % 3 === 0) {
       result += "fizz";
     }
+
     if (i % 5 === 0) {
       result += "buzz";
+    }
+
+    if (i % 7 === 0) {
+      result += "bang";
+    }
+
+    if (i % 11 === 0) {
+      result += "bong";
+    }
+
+    if (i % 13 === 0) {
+      const firstB = result.search("b");
+      if (firstB === -1) {
+        result = result + "Fezz";
+      } else {
+        resultFirstSplit = result.slice(0, firstB);
+        resultSecondSplit = result.slice(firstB);
+        resultIfB = "Fezz";
+        result = resultFirstSplit + resultIfB + resultSecondSplit;
+      }
     }
     if (result === "") {
       result = i;
@@ -17,7 +39,6 @@ function fizzbuzz() {
     console.log(result);
   }
 }
-
 // Now we run the main function...
 
 fizzbuzz();
