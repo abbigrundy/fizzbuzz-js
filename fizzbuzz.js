3200084f4bfe29408234637ec5fcb947fdb82f79
Abigail Grundy <abigial.grundy@bbc.co.uk>
2020-06-24 11:21:37 +0100

    part one completed

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,23 @@
-
-
 // Here, we create our main function.
+
 function fizzbuzz() {
-    
-    console.log('Welcome to FizzBuzz!');
-
-    // Put your code here...
-
-    
+  console.log("Welcome to Fizzbuzz");
+  //put your code here
+  for (let i = 0; i <= 100; i++) {
+    let result = "";
+    if (i % 3 === 0) {
+      result += "fizz";
+    }
+    if (i % 5 === 0) {
+      result += "buzz";
+    }
+    if (result === "") {
+      result = i;
+    }
+    console.log(result);
+  }
 }
 
 // Now we run the main function...
+
 fizzbuzz();
